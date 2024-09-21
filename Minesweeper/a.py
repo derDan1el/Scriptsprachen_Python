7b30e28c40ba4210e4d0fa8e36ba1b7407815fbb
Daniel Schicker <schicker.daniel@gmx.net>
2024-09-21 02:49:56 +0200

    deleting push

--- Minesweeper/a.py
+++
@@ -1,6 +0,0 @@
-class Dummy:
-    def __init__(self):
-        pass
-
-    def dummy_method(self):
-        pass
--- Minesweeper/b.py
+++
@@ -1,7 +0,0 @@
-from a import Dummy
-class KI_Minesweeper(buttons = []):
-    def __init__(self, buttons):
-        self.buttons = buttons
-        self.dummy = Dummy()
-    def get_next_move(self):
-        return self.dummy.get_next_move()
